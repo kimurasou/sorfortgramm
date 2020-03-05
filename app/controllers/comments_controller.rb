2b4c66cc6a4c7bbea3801873a269b92b6fc9aa09
kimurasou <cb.102444@gmail.com>
2020-03-05 20:14:42 +0900

    コメント

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])  
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])  
   end
 end
--- db/migrate/20200302130435_create_comments.rb
+++ db/migrate/20200302130435_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+      t.text :text
       t.integer :user_id
       t.integer :post_id
-      t.text :text, null: false
       t.timestamps
     end
   end
